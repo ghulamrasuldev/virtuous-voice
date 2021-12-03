c6036102debdfa729670b0f5bc80acff958b76ea
ghulamrasuldev <ghulam.rasool.development@gmail.com>
2021-12-03 21:31:23 +0500

    create sign in screen

--- app/src/main/java/com/example/virtuousvoice/WelcomeScreen.kt
+++ app/src/main/java/com/example/virtuousvoice/WelcomeScreen.kt
@@ -1,12 +1,34 @@
 package com.example.virtuousvoice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome_screen.*
 
 class WelcomeScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome_screen)
-        val intent = Intent(this, )
+
+        //Signing in as Parent
+        _sign_in_as_parent.setOnClickListener{
+            val intent = Intent(this, SignIn::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        //Signing in as Child
+        _sign_in_as_child.setOnClickListener{
+            val intent = Intent(this, SignIn::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        //Signing in as Individual
+        _sign_in_as_individual.setOnClickListener{
+            val intent = Intent(this, SignIn::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
