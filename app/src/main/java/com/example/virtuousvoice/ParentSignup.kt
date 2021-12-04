24dd1fb83284fd56310b294373e0abfac106d31f
ghulamrasuldev <ghulam.rasool.development@gmail.com>
2021-12-04 15:43:17 +0500

    creating parent signup

---
+++ app/src/main/java/com/example/virtuousvoice/ParentSignup.kt
@@ -0,0 +1,11 @@
+package com.example.virtuousvoice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ParentSignup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_parent_signup)
+    }
+}
