09f2f1b38981782f86cc2d213148b8ce8bb1ce06
ghulamrasuldev <ghulam.rasool.development@gmail.com>
2021-12-07 12:50:53 +0500

    Creating Tabbed activity

---
+++ app/src/main/java/com/example/virtuousvoice/TabbedActivity.kt
@@ -0,0 +1,11 @@
+package com.example.virtuousvoice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TabbedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tabbed)
+    }
+}
